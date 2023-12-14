9057cb3ff844d53558f000fc98ea1685e387ebd7
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2023-12-14 20:47:02 +0200

    New Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import Header from "./Header_Page/Header";
 import QuickAccess from "./Quick_Access/QuickAccess"
 //import Subjects from "./Subjects/Subjects";
-//import Subjects from "./Subjects/Subjects";
-//import Login from "./Login_Page/Login"
+import Subjects from "./Subjects/Subjects";
+import Login from "./Login_Page/Login"
 import MainPage from "./Main_Page/Main_Page";
 //import Test from "./Test_Page/Test";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -12,18 +12,23 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 export default function App(){
 
-
   return (
+    <>
     <Router>
-      <>
+      
       <Header/>
-      <Routes>  
-        <Route path="/" element={<MainPage/>} />           
-        <Route path="/QuickAccessStud" element={<QuickAccess />} />
+      <Routes>
+        <Route path="/*" element={<MainPage/>} />           
+        <Route path="/QuickAccessStud/*" element={<QuickAccess />} />
+        <Route path="/QuickAccessStud/Subjects" element={<Subjects/>}/>
+        <Route path="/QuickAccessStud/Scorecard" element={<Login/>}/>
+        <Route path="/QuickAccessStud/Certificates" element={<Login/>}/>
+        <Route path="/QuickAccessStud/CoursesDeclaration" element={<Login/>}/>
+        <Route path="/QuickAccessStud/ActionHistory" element={<Login/>}/> 
       </Routes>
-    </> 
     </Router>
-  )
+    </>
+  );
 };
 
 
