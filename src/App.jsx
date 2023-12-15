59507b1e0734339871f256f2d60e3ad86f6c73c2
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2023-12-15 13:38:30 +0200

    Route update

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import Header from "./Header_Page/Header";
 import QuickAccess from "./Quick_Access/QuickAccess"
 //import Subjects from "./Subjects/Subjects";
-import Subjects from "./Subjects/Subjects";
+//import Subjects from "./Subjects/Subjects";
 import Login from "./Login_Page/Login"
 import MainPage from "./Main_Page/Main_Page";
-//import Test from "./Test_Page/Test";
+import Test from "./Test_Page/Test";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
@@ -20,7 +20,7 @@ export default function App(){
       <Routes>
         <Route path="/*" element={<MainPage/>} />           
         <Route path="/QuickAccessStud/*" element={<QuickAccess />} />
-        <Route path="/QuickAccessStud/Subjects" element={<Subjects/>}/>
+        <Route path="/QuickAccessStud/Subjects" element={<Test/>}/>
         <Route path="/QuickAccessStud/Scorecard" element={<Login/>}/>
         <Route path="/QuickAccessStud/Certificates" element={<Login/>}/>
         <Route path="/QuickAccessStud/CoursesDeclaration" element={<Login/>}/>
