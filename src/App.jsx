fd033292de2050874a6a2780cadf40dcf3418dfb
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2023-12-17 00:02:44 +0200

    Added Scorecard and Courses pages for the Students button

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css"
 import React from "react";
 import Header from "./Header_Page/Header";
 import QuickAccess from "./Quick_Access/QuickAccess"
-//import Subjects from "./Subjects/Subjects";
+import CoursesDec from "./Courses_Declaration/CoursesDec"
 import Subjects from "./Subjects/Subjects";
 import Login from "./Login_Page/Login"
 import MainPage from "./Main_Page/Main_Page";
-import Test from "./Test_Page/Test";
-//import Scorecard from "./Scorecard/Scorecard"
+import Scorecard from "./Scorecard/Scorecard";
+//import Test from "./Test_Page/Test";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 
 export default function App(){
@@ -22,9 +23,9 @@ export default function App(){
         <Route path="/*" element={<MainPage/>} />           
         <Route path="/QuickAccessStud/*" element={<QuickAccess />} />
         <Route path="/QuickAccessStud/Subjects" element={<Subjects/>}/>
-        <Route path="/QuickAccessStud/Scorecard" element={<Test/>}/>
+        <Route path="/QuickAccessStud/Scorecard" element={<Scorecard/>}/>
         <Route path="/QuickAccessStud/Certificates" element={<Login/>}/>
-        <Route path="/QuickAccessStud/CoursesDeclaration" element={<Login/>}/>
+        <Route path="/QuickAccessStud/CoursesDeclaration" element={<CoursesDec/>}/>
         <Route path="/QuickAccessStud/ActionHistory" element={<Login/>}/> 
       </Routes>
     </Router>
