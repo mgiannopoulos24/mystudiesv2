f37201a58f47d2a535611591e6257d52511b9d24
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2023-12-16 14:29:34 +0200

    Used Test.jsx to for checking

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import React from "react";
 import Header from "./Header_Page/Header";
 import QuickAccess from "./Quick_Access/QuickAccess"
 //import Subjects from "./Subjects/Subjects";
-//import Subjects from "./Subjects/Subjects";
+import Subjects from "./Subjects/Subjects";
 import Login from "./Login_Page/Login"
 import MainPage from "./Main_Page/Main_Page";
 import Test from "./Test_Page/Test";
+//import Scorecard from "./Scorecard/Scorecard"
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
@@ -20,8 +21,8 @@ export default function App(){
       <Routes>
         <Route path="/*" element={<MainPage/>} />           
         <Route path="/QuickAccessStud/*" element={<QuickAccess />} />
-        <Route path="/QuickAccessStud/Subjects" element={<Test/>}/>
-        <Route path="/QuickAccessStud/Scorecard" element={<Login/>}/>
+        <Route path="/QuickAccessStud/Subjects" element={<Subjects/>}/>
+        <Route path="/QuickAccessStud/Scorecard" element={<Test/>}/>
         <Route path="/QuickAccessStud/Certificates" element={<Login/>}/>
         <Route path="/QuickAccessStud/CoursesDeclaration" element={<Login/>}/>
         <Route path="/QuickAccessStud/ActionHistory" element={<Login/>}/> 
