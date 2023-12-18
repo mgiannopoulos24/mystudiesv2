2edf025876f5391169ffbc0e5c00708e2206aab0
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2023-12-18 18:24:10 +0200

    Added Test Login

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,15 @@ import Login from "./Login_Page/Login"
 import MainPage from "./Main_Page/Main_Page";
 import Scorecard from "./Scorecard/Scorecard";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { firebaseConfig } from './Firebase/firebase';
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
 // import Test from "./Test_Page/Test";
 
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 
 export default function App(){
@@ -25,7 +32,7 @@ export default function App(){
         <Route path="/QuickAccessStud/Subjects" element={<Subjects/>}/>
         <Route path="/QuickAccessStud/Scorecard" element={<Scorecard/>}/>
         <Route path="/QuickAccessStud/CoursesDeclaration" element={<CoursesDec/>}/>
-        <Route path="/QuickAccessStud/Certificates" element={<Login/>}/>
+        <Route path="/QuickAccessStud/Certificates" element={<Test db={db}/>}/>
         <Route path="/QuickAccessStud/ActionHistory" element={<Login/>}/> 
       </Routes>
     </Router>
