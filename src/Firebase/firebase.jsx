08b9413a06de41bf49601e302750853a96dd1cc8
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2023-12-18 18:23:30 +0200

    Added config

---
+++ src/Firebase/firebase.jsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9dOPa7x4wseD87y3yp3nkCFDCale8Q8g",
+    authDomain: "mystudiesv2.firebaseapp.com",
+    projectId: "mystudiesv2",
+    storageBucket: "mystudiesv2.appspot.com",
+    messagingSenderId: "630228654803",
+    appId: "1:630228654803:web:a64c379b27d0033173d226",
+    measurementId: "G-W0T225FS8Y"
+  };
+
+  
