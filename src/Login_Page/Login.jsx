30d235f14ee033c0d51e919792fce6e9ba82d91b
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2023-12-18 20:20:44 +0200

    Handle Login

--- src/Login_Page/Login.jsx
+++ src/Login_Page/Login.jsx
@@ -2,8 +2,40 @@ import "./Login.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { doc, getDoc } from 'firebase/firestore'
+const Login =({db}) => {
 
-const Login =() => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+      // Handles the login functionality of the user
+    async function handleLogin (e){
+        e.preventDefault()
+        
+        // We create a doc that 'points' at collection 'users' with primary key user's input email 
+        const ref = doc(db, "users", email); 
+        // Now "Bring me, from the collection 'users' the document with name/value 'email'"
+        const res = await getDoc(ref);
+
+        //If the user with email = "email" and password = "passowrd" exists in the db...
+        if (res.exists() && res.data().email === email && res.data().password === password) {
+            // Get the role and email...
+            const user_role = res.data().role
+            const user_email = res.data().email
+
+            // Store the email and role as keys in your browser local storage
+            localStorage.setItem('role', user_role)
+            localStorage.setItem('email', user_email)
+
+            // Go to page /courses
+            window.location.href = './courses'
+            console.log("Found User:", res.data());
+        } else {
+            console.log("No such document!");
+        }
+    }
+
     return (
         <>
         <div className="row-intro">
@@ -17,11 +49,11 @@ const Login =() => {
                 <div className="inputs">
                     <div className="input-user">
                         <FontAwesomeIcon icon={faUser} />
-                        <input type="text" placeholder="Όνομα Χρήστη" />
+                        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                     <div className="input-psw">
                         <FontAwesomeIcon icon={faLock} />
-                        <input type="password" placeholder="Κωδικός Πρόσβασης" />
+                        <input type="password" placeholder="Κωδικός Πρόσβασης" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                 </div>
                 <div className="support-container">
