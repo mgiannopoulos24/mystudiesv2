a70de03c7ec793a11a7b8ff8b92c8395b9be4d2f
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2023-12-23 20:30:23 +0200

    Profile Page

--- src/ProfileStud/ProfileStud.jsx
+++ src/ProfileStud/ProfileStud.jsx
@@ -1,13 +1,49 @@
 import React from 'react';
 import "./ProfileStud.css"
-
+import user_img from "./assets/user.png"
+import Grid from '@mui/material/Grid';
 
 export const ProfileStud =()=>{
     return(
-        
+        <>
         <div className='row-header'>
             <h2>Το προφίλ μου</h2>
         </div>
+        <div className='row-main-1'>
+            <img src={user_img} alt="user"/>
+        </div>
+        <div className='row-name-status'>
+            <h3>ΛΑΚΗΣ ΛΑΛΑΚΗΣ</h3>
+            Ενεργός
+        </div>
+        <div className='track-progress'>
+            <li className='done'>
+                Οκτώβριος 2023            
+            </li> 
+            <li className='done'>
+                Εξάμηνο 1
+            </li>
+            <li className='todo'>
+                Εξάμηνο 2
+            </li>
+            <li className='todo'>
+                Εξάμηνο 3
+            </li>
+        </div>
+        <div className='empty-space'></div>
+        <div className='row-info'>
+            <Grid container textAlign="center" rowSpacing={1} columnSpacing={{ xs: 1}}>
+                <Grid item xs={3}>Τμήμα</Grid>
+                <Grid item xs={3}>ΤΜΗΜΑ ΠΛΗΡΟΦΟΡΙΚΗΣ</Grid>
+                <Grid item xs={3}>Κατεύθυνση</Grid>
+                <Grid item xs={3}>ΚΟΡΜΟΣ</Grid>
+                <Grid item xs={3}></Grid>
+                <Grid item xs={3}></Grid>
+                <Grid item xs={3}>Πρόγραμμα Σπουδών</Grid>
+                <Grid item xs={3}><a href='#'>Πρόγραμμα Σπουδών 2023-24</a></Grid>
+            </Grid>
+        </div>
+        </>
     )
     
        
