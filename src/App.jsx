c306447f9a5eee36cb68f92e3159e8bebb7d181c
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2023-12-23 20:30:32 +0200

    Fixed Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import "./App.css"
 import React from "react";
 import Header from "./Header_Page/Header";
-import QuickAccess from "./Quick_Access/QuickAccess"
-import CoursesDec from "./Courses_Declaration/CoursesDec"
-import Subjects from "./Subjects/Subjects";
 import Login from "./Login_Page/Login"
-import MainPage from "./Main_Page/Main_Page";
-import Scorecard from "./Scorecard/Scorecard";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ProfileStud from "./ProfileStud/ProfileStud";
 // import firebaseApp from "./Firebase/firebase"
@@ -19,22 +14,15 @@ export default function App(){
 
   return (
     <>
-    <Header/>
-    {/* <Router>
+    <Router>
       
       <Header/>
       <Routes>
         <Route path="/*" element={<Login/>} />           
-        <Route path="/Profile" element={<Profile/>} />
-        <Route path="/QuickAccessStud/Subjects" element={<Subjects/>}/>
-        <Route path="/QuickAccessStud/Scorecard" element={<Scorecard/>}/>
-        <Route path="/QuickAccessStud/CoursesDeclaration" element={<CoursesDec/>}/>
-        <Route path="/QuickAccessStud/Certificates" element={<Login/>}/>
-        <Route path="/QuickAccessStud/ActionHistory" element={<Login/>}/> 
-        <Route path="/Login" element={<Login/>}/> 
+        {/* <Route path="/Main" element={<MainPage/>} /> */}
+        <Route path="/ProfileStud" element={<ProfileStud/>}/>
       </Routes>
-    </Router> */}
-    <ProfileStud/>
+    </Router>
     </>
   );
 
