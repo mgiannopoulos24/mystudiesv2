3951411a353a5828c1f2d73933d967f0e0dc6d5a
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2024-01-04 14:12:00 +0200

    Initial Commit with redirection to Main Page

---
+++ src/Login_Page/Login_Form/Login_Form.jsx
@@ -0,0 +1,61 @@
+import React,{useState} from "react"
+import {auth} from "../../Firebase/firebase"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { Link } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+
+const Login_Form=({onLoginSuccess})=>{
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+
+    const Login_In=(e)=>{
+        e.preventDefault();
+        signInWithEmailAndPassword(auth,email,password)
+        .then((userCredential)=>{
+            console.log(userCredential);
+            onLoginSuccess();
+        }).catch((error)=> {
+            console.log(error);
+        })
+        onLoginSuccess();
+    };
+    
+    return(
+        <>
+            <div className="login-form">
+                <form onSubmit={Login_In} style={{marginTop:"15px"}}>
+                <div className="row-email">
+                    <TextField
+                        label="Email"
+                        type="email"
+                        variant="outlined"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        margin="normal"
+                        sx={{ height: "15px",width:"400px"}} 
+                    />
+                </div>
+                <br></br>
+                <div className="row-password">
+                    <TextField
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        margin="normal"
+                        sx={{ height: "15px",width:"400px"}} 
+                    />
+                </div>
+                <br></br>
+                <div className="row-submit" style={{marginTop:"30px"}}>
+                    <Link to="/MainStud"><button type="submit">Είσοδος</button></Link>
+                </div>
+                </form>
+            </div>
+        
+        </>
+    )
+}
+
+export default Login_Form;
