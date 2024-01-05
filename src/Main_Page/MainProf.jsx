603d22da2bc26bf2cffad07c60f9efeb019e6b13
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2024-01-05 14:31:25 +0200

    Fixed main prof page

---
+++ src/Main_Page/MainProf.jsx
@@ -0,0 +1,15 @@
+import HeaderProf from "../Header_Page/HeaderProf";
+
+const MainProf=()=>{
+    return(
+
+        <>
+        <HeaderProf/>
+        <div className="row-prof">
+            <h1>Hello</h1>
+        </div>
+        </>
+    )
+}
+
+export default MainProf;
