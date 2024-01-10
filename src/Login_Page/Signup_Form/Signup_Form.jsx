ea6b168ed1934bfc127b1e28a743686e616e7521
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2024-01-10 13:34:25 +0200

    Display success message and close the form

--- src/Login_Page/Signup_Form/Signup_Form.jsx
+++ src/Login_Page/Signup_Form/Signup_Form.jsx
@@ -1,32 +1,46 @@
-import React,{useState} from "react"
+import React,{useState,useEffect } from "react"
 import {auth} from "../../Firebase/firebase"
 import { createUserWithEmailAndPassword} from "firebase/auth";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./Signup.css"
 
-const Signup_Form=()=>{
+const Signup_Form=({ onSignupSuccess })=>{
     const [email,setEmail]=useState('');
     const [password,setPassword]=useState('');
     const [showSignupNotification, setShowSignupNotification] = useState(false);
+    const [showSignupForm, setShowSignupForm] = useState(true); 
     const Sign_Up=(e)=>{
         e.preventDefault();
         createUserWithEmailAndPassword(auth,email,password)
         .then((userCredential)=>{
             console.log(userCredential);
             setShowSignupNotification(true);
+            setShowSignupForm(false);
         }).catch((error)=> {
             console.log(error);
         })
     };
-    const handleSignupNotificationClose = () => {
-        // Close the signup notification and reset the state
-        setShowSignupNotification(false);
-    };
     
     
+    useEffect(() => {
+        let timeoutId;
+        if (showSignupNotification) {
+          timeoutId = setTimeout(() => {
+            setShowSignupForm(false);
+            setShowSignupNotification(false);
+            onSignupSuccess();
+          }, 2000);
+        }
+      
+        return () => {
+          clearTimeout(timeoutId);
+        };
+      }, [showSignupNotification,onSignupSuccess]);
+
     return(
         <>
+        {showSignupForm && (
             <div className="signup-form">
                 <form onSubmit={Sign_Up} style={{marginTop:"30px"}}>
                 <div className="row-email">
@@ -60,10 +74,11 @@ const Signup_Form=()=>{
                 </div>                    
                 </form>
             </div>
+        )}
+            
             {showSignupNotification && (
-                <div className="signup-notification">
+                <div className={`signup-notification ${showSignupNotification ? 'show' : ''}`}>
                     <p style={{ color: "green" }}>Ο λογαριασμός δημιουργήθηκε με επιτυχία! Συνδεθείτε με τα στοιχεία σας.</p>
-                    <button onClick={handleSignupNotificationClose}>Κλείσιμο</button>
                 </div>
             )}
         
