ec350d851a33e4fef1a828c757d07cdce0cab538
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2024-01-12 17:12:48 +0200

    Changed the table values, made dynamic h4 tags and centered ul options

--- src/Declarations/DecHistory/DecHistory.jsx
+++ src/Declarations/DecHistory/DecHistory.jsx
@@ -14,32 +14,32 @@ import Breadcrumbs from "../../Breadcrumbs/Breadcrumbs";
 
 
 
-function createData(subject, id, professor, period, type,points) {
-    return {subject, id, professor, period, type,points};
+function createData(subject, id, professor, type,points) {
+    return {subject, id, professor,  type,points};
 }
 
 const rowsSem1 = [
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'Υ', 6),
 ];
 
 const rowsSem2 = [
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΑΝΑΛΥΣΗ 2', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'Υ', 6),
+    createData('ΑΝΑΛΥΣΗ 2', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ','Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'Υ', 6),
 ];
 
 const rowsSem3 = [
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΑΝΤΙΚΕΙΜΕΝΟΣΤΡΑΦΗΣ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6, 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
-    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ', 'ΣΕΠΤΕΜΒΡΙΟΣ 2023-24', 'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ',  'Υ', 6),
+    createData('ΑΝΤΙΚΕΙΜΕΝΟΣΤΡΑΦΗΣ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ',  'Υ', 6, ),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ',  'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ',  'Υ', 6),
+    createData('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ', 'Κ08', 'ΑΥΓΕΡΙΝΟΣ',  'Υ', 6),
 ];
 
 
@@ -64,34 +64,32 @@ const DecHistory=()=>{
             </div><br></br>
             <div className="two-col-dhis">
                 <div className="col-dhis-1">
-                    <ul style={{listStyle:"none",fontSize:18}}>
+                    <ul style={{listStyle:"none",fontSize:18,marginLeft:"20px"}}>
                         <li style={{ cursor: "pointer" }} onClick={() => handleYearChange('2023-24')}>2023-24</li>
                         <li style={{ cursor: "pointer" }} onClick={() => handleYearChange('2022-23')}>2022-23</li>
                     </ul>
                 </div>
                 <div className="col-dhis-2">
                     <Card style={{width:"98%",alignItems:"center"}}>
-                        <h4 style={{marginLeft:"10px"}}>ΧΕΙΜΕΡΙΝΟ ΕΞΑΜΗΝΟ 2023-24</h4>
+                        <h4 style={{marginLeft:"10px"}}>{selectedYear === '2023-24' ? 'ΧΕΙΜΕΡΙΝΟ ΕΞΑΜΗΝΟ 2023-24' : 'ΧΕΙΜΕΡΙΝΟ ΕΞΑΜΗΝΟ 2022-23'}</h4>
                         <div className="row-table-dhis-1" style={{ display: selectedYear === '2023-24' ? 'block' : 'none' }}>
                         <TableContainer sx={{margin: "0 25px"}}>
                             <Table sx={{width:"100%"}}>
                                 <TableHead >
                                     <TableRow>
                                         <TableCell style={{ width: '20%', fontWeight: 'bold'}}>Μάθημα</TableCell>
-                                        <TableCell style={{ width: '5%', fontWeight: 'bold' }}>Κωδικός</TableCell>
-                                        <TableCell style={{ width: '10%', fontWeight: 'bold' }}>Διδάσκων</TableCell>
-                                        <TableCell style={{ width: '10%' , fontWeight: 'bold'}}>Εξεταστική</TableCell>
-                                        <TableCell style={{ width: '5%' , fontWeight: 'bold'}}>Τύπος</TableCell>
-                                        <TableCell style={{ width: '3%' , fontWeight: 'bold'}}>ΔΜ</TableCell>
+                                        <TableCell style={{ width: '20%', fontWeight: 'bold' }}>Κωδικός</TableCell>
+                                        <TableCell style={{ width: '20%', fontWeight: 'bold' }}>Διδάσκων</TableCell>
+                                        <TableCell style={{ width: '15%' , fontWeight: 'bold'}}>Τύπος</TableCell>
+                                        <TableCell style={{ width: '5%' , fontWeight: 'bold'}}>ΔΜ</TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody sx={{border: '2px solid #ddd'}}>
-                                    {rowsSem3.map((row)=>(
+                                    {rowsSem1.map((row)=>(
                                     <TableRow key={row.name}>
                                         <TableCell>{row.subject}</TableCell>
                                         <TableCell>{row.id}</TableCell>
                                         <TableCell>{row.professor}</TableCell>
-                                        <TableCell>{row.period}</TableCell>
                                         <TableCell>{row.type}</TableCell>
                                         <TableCell>{row.points}</TableCell>
                                     </TableRow>))}
@@ -105,39 +103,10 @@ const DecHistory=()=>{
                                 <TableHead >
                                     <TableRow>
                                         <TableCell style={{ width: '20%', fontWeight: 'bold'}}>Μάθημα</TableCell>
-                                        <TableCell style={{ width: '5%', fontWeight: 'bold' }}>Κωδικός</TableCell>
-                                        <TableCell style={{ width: '10%', fontWeight: 'bold' }}>Διδάσκων</TableCell>
-                                        <TableCell style={{ width: '10%' , fontWeight: 'bold'}}>Εξεταστική</TableCell>
-                                        <TableCell style={{ width: '5%' , fontWeight: 'bold'}}>Τύπος</TableCell>
-                                        <TableCell style={{ width: '3%' , fontWeight: 'bold'}}>ΔΜ</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody sx={{border: '2px solid #ddd'}}>
-                                    {rowsSem2.map((row)=>(
-                                    <TableRow key={row.name}>
-                                        <TableCell>{row.subject}</TableCell>
-                                        <TableCell>{row.id}</TableCell>
-                                        <TableCell>{row.professor}</TableCell>
-                                        <TableCell>{row.period}</TableCell>
-                                        <TableCell>{row.type}</TableCell>
-                                        <TableCell>{row.points}</TableCell>
-                                    </TableRow>))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        </div><br></br>
-                        <h4 style={{marginLeft:"10px"}}>ΕΑΡΙΝΟ ΕΞΑΜΗΝΟ 2022-23</h4>
-                        <div className="row-table-dhis-3" style={{ display: selectedYear === '2023-24' ? 'block' : 'none' }}>
-                        <TableContainer sx={{margin: "0 25px"}}>
-                            <Table sx={{width:"100%"}}>
-                                <TableHead >
-                                    <TableRow>
-                                        <TableCell style={{ width: '20%', fontWeight: 'bold'}}>Μάθημα</TableCell>
-                                        <TableCell style={{ width: '5%', fontWeight: 'bold' }}>Κωδικός</TableCell>
-                                        <TableCell style={{ width: '10%', fontWeight: 'bold' }}>Διδάσκων</TableCell>
-                                        <TableCell style={{ width: '10%' , fontWeight: 'bold'}}>Εξεταστική</TableCell>
-                                        <TableCell style={{ width: '5%' , fontWeight: 'bold'}}>Τύπος</TableCell>
-                                        <TableCell style={{ width: '3%' , fontWeight: 'bold'}}>ΔΜ</TableCell>
+                                        <TableCell style={{ width: '20%', fontWeight: 'bold' }}>Κωδικός</TableCell>
+                                        <TableCell style={{ width: '20%', fontWeight: 'bold' }}>Διδάσκων</TableCell>
+                                        <TableCell style={{ width: '15%' , fontWeight: 'bold'}}>Τύπος</TableCell>
+                                        <TableCell style={{ width: '5%' , fontWeight: 'bold'}}>ΔΜ</TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody sx={{border: '2px solid #ddd'}}>
@@ -146,7 +115,6 @@ const DecHistory=()=>{
                                         <TableCell>{row.subject}</TableCell>
                                         <TableCell>{row.id}</TableCell>
                                         <TableCell>{row.professor}</TableCell>
-                                        <TableCell>{row.period}</TableCell>
                                         <TableCell>{row.type}</TableCell>
                                         <TableCell>{row.points}</TableCell>
                                     </TableRow>))}
@@ -154,6 +122,35 @@ const DecHistory=()=>{
                             </Table>
                         </TableContainer>
                         </div><br></br>
+                        <h4 style={{marginLeft:"10px"}}>{selectedYear === '2023-24' ? 'ΕΑΡΙΝΟ ΕΞΑΜΗΝΟ 2023-24' : 'ΕΑΡΙΝΟ ΕΞΑΜΗΝΟ 2022-23'}</h4>
+                        <div className="row-table-dhis-3" style={{ display: selectedYear === '2022-23' ? 'block' : 'none'  }}>
+                        
+                        </div><br></br>
+                        <div className="row-table-dhis-4" style={{ display: selectedYear === '2022-23' ? 'block' : 'none' }}>
+                        <TableContainer sx={{margin: "0 25px"}}>
+                            <Table sx={{width:"100%"}}>
+                                <TableHead >
+                                    <TableRow>
+                                        <TableCell style={{ width: '20%', fontWeight: 'bold'}}>Μάθημα</TableCell>
+                                        <TableCell style={{ width: '20%', fontWeight: 'bold' }}>Κωδικός</TableCell>
+                                        <TableCell style={{ width: '20%', fontWeight: 'bold' }}>Διδάσκων</TableCell>
+                                        <TableCell style={{ width: '15%' , fontWeight: 'bold'}}>Τύπος</TableCell>
+                                        <TableCell style={{ width: '5%' , fontWeight: 'bold'}}>ΔΜ</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody sx={{border: '2px solid #ddd'}}>
+                                    {rowsSem2.map((row)=>(
+                                    <TableRow key={row.name}>
+                                        <TableCell>{row.subject}</TableCell>
+                                        <TableCell>{row.id}</TableCell>
+                                        <TableCell>{row.professor}</TableCell>
+                                        <TableCell>{row.type}</TableCell>
+                                        <TableCell>{row.points}</TableCell>
+                                    </TableRow>))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer><br></br>
+                        </div>
 
                     </Card>
                 </div>
