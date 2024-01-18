2ea49416fce85cea3acf6c78e789036235b59114
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2024-01-18 23:26:24 +0200

    Updated RGrades

--- src/Grades/Recent Grades/RGrades.jsx
+++ src/Grades/Recent Grades/RGrades.jsx
@@ -28,26 +28,11 @@ const RGrades=()=>{
                         <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                             <div style={{ flex: '50%',display: 'flex',alignItems: 'center'}}>
                             <CardContent sx={{fontSize:14}}>
-                            <h3 style={{ margin: 0 }}>Εισαγωγή στον Προγραμματισμό</h3>
-                            </CardContent>
-                            </div>
-                            <div style={{ flex: '50%',display: 'flex',alignItems: 'center' }}>
-                                <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>111<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΠΑΝΑΓΙΩΤΗΣ ΣΤΑΜΑΤΟΠΟΥΛΟΣ
-                            </div>
-                        </div>
-                        <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'green' }}>
-                            <h1>10</h1>
-                        </div>
-                    </Card><br></br>
-                    <Card sx={{width:'80%',display: 'flex',height:'150px'}}>
-                        <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
-                            <div style={{ flex: '50%',display: 'flex',alignItems: 'center'}}>
-                            <CardContent sx={{fontSize:14}}>
                             <h3 style={{ margin: 0 }}>Γραμμική Άλγεβρα</h3>
                             </CardContent>
                             </div>
                             <div style={{ flex: '50%',display: 'flex',alignItems: 'center' }}>
-                                <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>250<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΝΑΚΟΣ
+                                <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>250<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΑΡΧΟΝΤΙΑ ΓΙΑΝΝΟΠΟΥΛΟΥ
                             </div>
                         </div>
                         <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'green' }}>
@@ -58,15 +43,15 @@ const RGrades=()=>{
                         <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                             <div style={{ flex: '50%',display: 'flex',alignItems: 'center'}}>
                             <CardContent sx={{fontSize:14}}>
-                            <h3 style={{ margin: 0 }}>Εισαγωγή στις Τηλεπικοινωνίες</h3>
+                            <h3 style={{ margin: 0 }}>Ανάλυση Ι</h3>
                             </CardContent>
                             </div>
                             <div style={{ flex: '50%',display: 'flex',alignItems: 'center' }}>
-                                <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>124<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΜΑΡΙΑ ΡΟΥΣΣΟΥ
+                                <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>124<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΠΑΝΤΕΛΕΗΜΩΝ ΔΟΔΟΣ
                             </div>
                         </div>
-                        <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'green' }}>
-                            <h1>7</h1>
+                        <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'red' }}>
+                            <h1>3</h1>
                         </div>
                     </Card><br></br>
                     <Card sx={{width:'80%',display: 'flex',height:'150px'}}>
@@ -77,28 +62,28 @@ const RGrades=()=>{
                             </CardContent>
                             </div>
                             <div style={{ flex: '50%',display: 'flex',alignItems: 'center' }}>
-                                <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>67<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΒΑΣΙΛΕΙΟΣ ΚΑΡΑΚΩΣΤΑΣ
+                                <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>67<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΑΝΤΩΝΙΟΣ ΠΑΣΧΑΛΗΣ
                             </div>
                         </div>
-                        <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'red' }}>
-                            <h1>4</h1>
+                        <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'green' }}>
+                            <h1>10</h1>
                         </div>
                     </Card>
                 </div>
             </div>
             <div className="column-2-rgrade">
-                <Card sx={{width:"90%",height:"60%"}}>
+                <Card sx={{width:"90%",height:"90%"}}>
                     <div className="row-card-title"style={{display: 'flex',alignItems: 'center',justifyContent:"center",fontSize:18}}>
                         ΑΠΟΤΕΛΕΣΜΑΤΑ<br></br>
-                        Σεπτέμβριος 2023-24 
+                        Σεπτέμβριος 2023-24
                     </div><br></br>
                     <div className="row-round-progress" style={{display: 'flex',alignItems: 'center',justifyContent:"center"}}>
                         <div style={{ width: '150px', height: '150px' }}>
                             <CircularProgressbar 
-                            value={75} 
+                            value={66} 
                             text={
                                 <tspan>
-                                    <tspan x="50" dy="-0.4em" fontSize="18" fontWeight="bold">3/9</tspan>                                    
+                                    <tspan x="50" dy="-0.4em" fontSize="18" fontWeight="bold">2/3</tspan>                                    
                                     <tspan x="50" dy="1.2em" fontSize="13" fontWeight="normal">Περασμένα</tspan>
                                 </tspan>
                             }
@@ -110,16 +95,16 @@ const RGrades=()=>{
                     </div><br></br>
                     <div className="exam-sub">
                         <div className="d-cell-1"style={{flex:"90%",marginLeft:"20px"}}>Δηλωμένα μαθήματα</div>
-                        <div className="d-cell-2" style={{flex:"10%"}}>9</div>
+                        <div className="d-cell-2" style={{flex:"10%"}}>3</div>
                     </div>
                     <div className="passed-sub">
                         <div className="p-cell-1"style={{flex:"90%",marginLeft:"20px"}}>Περασμένα μαθήματα</div>
-                        <div className="p-cell-2" style={{flex:"10%"}}>3</div>
+                        <div className="p-cell-2" style={{flex:"10%"}}>2</div>
                     </div>
                     <hr></hr>
                     <div className="gpa">
                         <div className="gpa-cell-1"style={{flex:"90%",marginLeft:"20px",alignItems:'center',justifyContent:"center",marginTop:'10px'}}>Μέσος όρος</div>
-                        <div className="gpa-cell-2" style={{flex:"10%",fontSize:"30px",alignItems:'center',justifyContent:"center"}}>10</div>
+                        <div className="gpa-cell-2" style={{flex:"10%",fontSize:"30px",alignItems:'center',justifyContent:"center",marginRight:"5px"}}>6</div>
                     </div>
                 </Card>
             </div>
