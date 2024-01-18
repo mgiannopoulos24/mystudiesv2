a2d134839c9fa75845c9242233d90c8b7cdbcb5a
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2024-01-18 21:21:12 +0200

    Made cards bigger

--- src/Main_Page/MainStud.jsx
+++ src/Main_Page/MainStud.jsx
@@ -9,6 +9,7 @@ import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
+import BackToTop from "../BackToTop/BackToTop";
 
 const MainStud = () =>{
     const AllCourses = 'https://www.di.uoa.gr/studies/undergraduate/courses';
@@ -16,6 +17,7 @@ const MainStud = () =>{
         <>
         <Header/>
         <Breadcrumbs/>
+        <BackToTop/>
         <div className="row-header">
             <img src={user_img} alt="user"/><span><h2>Αρχική Σελίδα</h2></span>
         </div>
@@ -52,9 +54,9 @@ const MainStud = () =>{
                 <div className="row-subject">
                     <h3>Τα μαθήματα μου</h3><br></br>
                     ΧΕΙΜΕΡΙΝΟ ΕΞΑΜΗΝΟ 2023-24<br></br>
-                    <strong className="stud-prog">Τρέχων Εξάμηνο</strong><Link to={AllCourses}><Button variant="contained" sx={{marginLeft:'477px',textTransform:"none",marginBottom:"20px"}}>'Ολα τα μαθήματα</Button></Link>
+                    <strong className="stud-prog">Τρέχων Εξάμηνο</strong><Link to={AllCourses}><Button variant="contained" sx={{marginLeft:'707px',textTransform:"none",marginBottom:"20px"}}>'Ολα τα μαθήματα</Button></Link>
                 </div>
-                <Card sx={{width:750,display: 'flex'}}>
+                <Card sx={{width:"95%",display: 'flex'}}>
                     <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                         <div style={{ flex: '33%',display: 'flex',alignItems: 'center'}}>
                         <CardContent sx={{fontSize:14}}>
@@ -66,13 +68,13 @@ const MainStud = () =>{
                         </div>
                         <div style={{ flex: '33%',display: 'flex',alignItems: 'center' }}>
                             <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>300
-                        </div>
+                        </div><br></br>
                     </div>
                     <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center'}}>
                         <Button variant="outlined" component={Link} to="https://eclass.uoa.gr/courses/DI632/"sx={{ borderRadius: '25px', textTransform:'none'}}>E-Class</Button>
                     </div>
                 </Card><br></br>
-                <Card sx={{width:750,display: 'flex'}}>
+                <Card sx={{width:"95%",display: 'flex'}}>
                     <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                         <div style={{ flex: '33%',display: 'flex',alignItems: 'center'}}>
                         <CardContent sx={{fontSize:14}}>
@@ -84,13 +86,13 @@ const MainStud = () =>{
                         </div>
                         <div style={{ flex: '33%',display: 'flex',alignItems: 'center' }}>
                             <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>260
-                        </div>
+                        </div><br></br>
                     </div>
                     <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center'}}>
                         <Button variant="outlined" component={Link} to="https://eclass.uoa.gr/courses/D260/"sx={{ borderRadius: '25px', textTransform:'none'}}>E-Class</Button>
                     </div>
                 </Card><br></br>
-                <Card sx={{width:750,display: 'flex'}}>
+                <Card sx={{width:"95%",display: 'flex'}}>
                     <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                         <div style={{ flex: '33%',display: 'flex',alignItems: 'center'}}>
                         <CardContent sx={{fontSize:14}}>
@@ -102,13 +104,13 @@ const MainStud = () =>{
                         </div>
                         <div style={{ flex: '33%',display: 'flex',alignItems: 'center' }}>
                             <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>180
-                        </div>
+                        </div><br></br>
                     </div>
                     <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center'}}>
                         <Button variant="outlined" component={Link} to="https://eclass.uoa.gr/courses/DI539/"sx={{ borderRadius: '25px', textTransform:'none'}}>E-Class</Button>
                     </div>
                 </Card><br></br>
-                <Card sx={{width:750,display: 'flex'}}>
+                <Card sx={{width:"95%",display: 'flex'}}>
                     <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                         <div style={{ flex: '33%',display: 'flex',alignItems: 'center'}}>
                         <CardContent sx={{fontSize:14}}>
@@ -120,19 +122,19 @@ const MainStud = () =>{
                         </div>
                         <div style={{ flex: '33%',display: 'flex',alignItems: 'center' }}>
                             <span style={{ marginLeft: '20px' }}/><FlagOutlinedIcon style={{fontSize: '15px'}}/>69
-                        </div>
+                        </div><br></br>
                     </div>
                     <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center'}}>
                         <Button variant="outlined" component={Link} to="https://eclass.uoa.gr/courses/DI617/"sx={{ borderRadius: '25px', textTransform:'none'}}>E-Class</Button>
                     </div>
-                </Card>
+                </Card><br></br>
             </div>
             <div className="column-grades">
                 <div className="row-grades">
                     <h3>Οι πρόσφατοι βαθμοί μου</h3><br></br>
                     Σεπτέμβριος 2023-24
                 </div><br></br>
-                <Card sx={{width:500,display: 'flex'}}>
+                <Card sx={{width:800,display: 'flex'}}>
                     <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                         <div style={{ flex: '50%',display: 'flex',alignItems: 'center'}}>
                         <CardContent sx={{fontSize:14}}>
@@ -141,13 +143,13 @@ const MainStud = () =>{
                         </div>
                         <div style={{ flex: '50%',display: 'flex',alignItems: 'center' }}>
                             <span style={{ marginLeft: '20px' }}/><PersonIcon style={{fontSize: '15px'}}/>111<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΠΑΝΑΓΙΩΤΗΣ ΣΤΑΜΑΤΟΠΟΥΛΟΣ
-                        </div>
+                        </div><br></br>
                     </div>
                     <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'green' }}>
                         <h1>10</h1>
                     </div>
                 </Card><br></br>
-                <Card sx={{width:500,display: 'flex'}}>
+                <Card sx={{width:800,display: 'flex'}}>
                     <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                         <div style={{ flex: '50%',display: 'flex',alignItems: 'center'}}>
                         <CardContent sx={{fontSize:14}}>
@@ -156,13 +158,13 @@ const MainStud = () =>{
                         </div>
                         <div style={{ flex: '50%',display: 'flex',alignItems: 'center' }}>
                             <span style={{ marginLeft: '20px' }}/><PersonIcon style={{fontSize: '15px'}}/>250<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΝΑΚΟΣ
-                        </div>
+                        </div><br></br>
                     </div>
                     <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'green' }}>
                         <h1>6</h1>
                     </div>
                 </Card><br></br>
-                <Card sx={{width:500,display: 'flex'}}>
+                <Card sx={{width:800,display: 'flex'}}>
                     <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                         <div style={{ flex: '50%',display: 'flex',alignItems: 'center'}}>
                         <CardContent sx={{fontSize:14}}>
@@ -171,13 +173,13 @@ const MainStud = () =>{
                         </div>
                         <div style={{ flex: '50%',display: 'flex',alignItems: 'center' }}>
                             <span style={{ marginLeft: '20px' }}/><PersonIcon style={{fontSize: '15px'}}/>124<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΜΑΡΙΑ ΡΟΥΣΣΟΥ
-                        </div>
+                        </div><br></br>
                     </div>
                     <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'green' }}>
                         <h1>7</h1>
                     </div>
                 </Card><br></br>
-                <Card sx={{width:500,display: 'flex'}}>
+                <Card sx={{width:800,display: 'flex'}}>
                     <div style={{ flex: '80%', display: 'flex', flexDirection: 'column'}}>
                         <div style={{ flex: '50%',display: 'flex',alignItems: 'center'}}>
                         <CardContent sx={{fontSize:14}}>
@@ -186,7 +188,7 @@ const MainStud = () =>{
                         </div>
                         <div style={{ flex: '50%',display: 'flex',alignItems: 'center' }}>
                             <span style={{ marginLeft: '20px' }}/><PersonIcon style={{fontSize: '15px'}}/>67<span style={{ marginLeft: '5px' }}/><SchoolIcon style={{fontSize:'15px'}}/><span style={{ marginLeft: '5px' }}/>ΒΑΣΙΛΕΙΟΣ ΚΑΡΑΚΩΣΤΑΣ
-                        </div>
+                        </div><br></br>
                     </div>
                     <div style={{ flex: '20%', display: 'flex',alignItems: 'center', justifyContent: 'center', color: 'red' }}>
                         <h1>4</h1>
