5e07e7914f7553b8a2898b8e9a1dd3e25afccf3f
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2024-01-20 20:32:09 +0200

    Added 2 new paths

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import DecHistory from "./Declarations/DecHistory/DecHistory";
 import MainProf from "./Main_Page/MainProf";
 import Books from "./Declarations/Books/Books";
 import Requests from "./Support/Requests";
+import OpenScoreCompilers from "./Scorecard/OpenScore/OpenScoreCompilers";
+import OpenScoreGraphics from "./Scorecard/OpenScore/OpenScoreGraphics";
 // import { getFirestore } from 'firebase/firestore'
 //const db = getFirestore(firebaseApp);
 
@@ -31,6 +33,10 @@ export default function App(){
         <Route path="/MainStud/Books" element={<Books/>}/>
         <Route path="/MainStud/Requests" element={<Requests/>}/>
         <Route path="/MainProf" element={<MainProf/>} />
+        <Route path="/MainProf/SubjectsOpenScore/Compilers" element={<OpenScoreCompilers/>}/>
+        <Route path="/MainProf/SubjectsOpenScore/Graphics" element={<OpenScoreGraphics/>}/>
+        {/* <Route path="/MainProf/SubjectsSubmittedScore/SubmittedScore/Arch" element={< />}/>
+        <Route path="/MainProf/SubjectsSubmittedScore/SubmittedScore/Syspro" element={< />}/> */}
       </Routes>
     </Router>
     </>
