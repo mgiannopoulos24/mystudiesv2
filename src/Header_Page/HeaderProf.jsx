c05cde9ac81cd563d3b9d045f6fc2270cabdf1ad
mgiannopoulos24 <sdi2000032@di.uoa.gr>
2024-01-20 20:29:17 +0200

    Added notifications and fixed stretch problem

--- src/Header_Page/HeaderProf.jsx
+++ src/Header_Page/HeaderProf.jsx
@@ -5,9 +5,10 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import { auth } from "../Firebase/firebase";
 import React, { useState, useEffect } from "react";
 import "./HeaderProf.css"
+import Badge from '@mui/material/Badge';
 
 const HeaderProf=()=>{
-    
+    const notificationsCount = 1;
     const [userEmail, setUserEmail] = useState(null);
 
     useEffect(() => {
@@ -27,33 +28,34 @@ const HeaderProf=()=>{
             <div className='column-1-header-1'>
                 <h2>My Studies</h2>
             </div>
-            <div className='column-2-header-1'>
-                        
-            </div>
             <div className='column-3-header-1'>
                 <div className='row-icons-1'>
                     <div className='cell-home-1'><a href="/MainProf" style={{ textDecoration: 'none', color: 'inherit' }}><HomeRoundedIcon style={{ fontSize: '30px' }}/></a></div>
                     <div className='cell-grades-1'>
                         <div className='dropdown-1'>
-                            <button className='dropbutton-1'>Βαθμολογία<span><ExpandMoreRoundedIcon style={{fontSize: '15px'}}/></span></button>
+                            <button className='dropbutton-1'><span className='button-text-1'>Βαθμολογία</span><ExpandMoreRoundedIcon style={{fontSize: '20px'}}/></button>
                             <div className='dropdown-content-1'>
-                                <a href=''>Πρόσφατη Βαθμολογία</a>
-                                <a href=''>Αναλυτική Βαθμολογία</a>
+                                <a href=''>Δημιουργία Βαθμολογίου</a>
+                                <a href=''>Παρακολούθηση Βαθμολογίου</a>
                             </div>
                         </div>
                     </div>                    
                     <div className='cell-notif-1'>
                         <div className='dropdown-1'>
-                            <button className='dropbutton-1' style={{padding:'8px',justifyContent:'center',alignItems:"center"}}><NotificationsIcon style={{fontSize: '30px'}}/></button>
+                            <button className='dropbutton-1' style={{padding:'8px',justifyContent:'center',alignItems:"center"}}>
+                                <Badge badgeContent={notificationsCount} overlap="circular" color="error" >
+                                    <NotificationsIcon style={{fontSize: '30px'}}/>
+                                </Badge>
+                            </button>
                             <div className="dropdown-content-1">
-                                <a href=''></a>
-                                <a href=''>Όλα τα μηνύματα</a>
+                                <a href=""><div>Καλημέρα σας. ΓΑΜΩ ΤΗΝ ΠΑΤΡΑ.</div></a>
                             </div>
+                           
                     </div>    
                     </div>
                     <div className='cell-user-1'>
                         <div className='dropdown-1'>
-                            <button className='dropbutton-1'>{userEmail}<span><ExpandMoreRoundedIcon style={{fontSize: '15px'}}/></span></button>
+                            <button className='dropbutton-1'><span className='button-text-1'>{userEmail}</span><ExpandMoreRoundedIcon style={{fontSize: '20px'}}/></button>
                             <div className='dropdown-content-1'>
                                 <a href=''>Το προφίλ μου</a>
                                 <a href='/Login'>Αποσύνδεση</a>
@@ -62,7 +64,7 @@ const HeaderProf=()=>{
                     </div>
                     <div className='cell-lang-1'>
                         <div className='dropdown-1'>
-                            <button className='dropbutton-1'>Ελληνικά<span><ExpandMoreRoundedIcon style={{fontSize: '15px'}}/></span></button>
+                            <button className='dropbutton-1'><span className='button-text-1'>Ελληνικά</span><ExpandMoreRoundedIcon style={{fontSize: '20px'}}/></button>
                             <div className='dropdown-content-1'>
                                 <option>Αγγλικά</option>
                             </div>
